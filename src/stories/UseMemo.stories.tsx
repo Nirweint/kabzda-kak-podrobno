2fb0527d0c75bc679ce6d89dcead812291ce5c87
Nirweint <nirweint_1@mail.ru>
2021-12-02 17:22:46 +0300

    useMemo and React.Memo usage

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 
 export default {
     title: 'useMemo',
@@ -12,9 +12,19 @@ export const Example1 = () => {
     let resultA = 1;
     let resultB = 1;
 
-    for (let i = 1; i <= a; i++) {
-        resultA = resultA * i
-    }
+   resultA = useMemo(() => {
+       let tempResultA = 1
+       for (let i = 1; i <= a; i++) {
+           let fake = 0;
+           while(fake < 10000000) {
+               fake ++;
+               const fakeValue = Math.random()
+           }
+           tempResultA = tempResultA * i
+       }
+       return tempResultA
+    }, [a])
+
     for (let i = 1; i <= b; i++) {
         resultB = resultB * i
     }
@@ -30,6 +40,42 @@ export const Example1 = () => {
             <div>
                 Result for b: {resultB}
             </div>
+        </div>
+    )
+}
+
+
+
+const UsersSecret = (props: {users: Array<string>}) => {
+    console.log("Users Render")
+    return (
+        <div>
+            {props.users.map((us, i) => {
+                return (
+                    <div key={i}>{us}</div>
+                )
+            })}
+        </div>
+    )
+}
+
+const Users = React.memo(UsersSecret)
+
+
+export const Example2 = () => {
+    console.log('example2 ')
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(["Alex", "Sofi", "Pasha", "Oleg", "Katya"])
+
+    const newArray = useMemo(() => {
+       return  users.filter(u => u.toLowerCase().indexOf('a') > -1)
+    }, [users])
+
+    return (
+        <div>
+            <button onClick={() => setCounter(counter + 1)} >+</button>
+            {counter}
+            <Users users={newArray}/>
         </div>
     )
 }
